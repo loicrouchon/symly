f8ba9979530e927efd0f1d2cec67df86944497f2
Loic Rouchon <loic.rouchon@gmail.com>
2021-02-13 22:53:04 +0100

    Migrate AddCommandTest messages asserts to AddMessageFactory

--- src/integration-test/java/org/linky/cli/AddCommandTest.java
+++ src/integration-test/java/org/linky/cli/AddCommandTest.java
@@ -1,12 +1,16 @@
 package org.linky.cli;
 
-import org.junit.jupiter.api.Disabled;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.linky.env.Env;
 import org.linky.env.IntegrationTest;
 import org.linky.files.FileTree.Diff;
 
 @SuppressWarnings("java:S2699")
 class AddCommandTest extends IntegrationTest {
+
+    private final AddMessageFactory msg = new AddMessageFactory(env);
 
     @Test
     void shouldFail_whenRequiredArgsAreMissing() {
@@ -16,22 +20,20 @@ class AddCommandTest extends IntegrationTest {
         whenRunningCommand("add")
                 .thenItShould()
                 .fail()
-                .withErrorMessage("Missing required options and parameters: '--to=<to>', '<file>'")
+                .withErrorMessage(msg.missingArguments())
                 .withFileTreeDiff(Diff.unchanged());
     }
 
     @Test
-    @Disabled("not yet implemented")
-    void shouldProvideCorrectDefaults() {
+    void shouldFail_whenDirectoryDoesNotExist() {
         //given
         given(env)
-                .withDirectories("to/dir")
-                .withFiles("some/file");
+                .withHome("home/doesnotexist");
         //when/then
-        whenRunningCommand("add", "-t", "to/dir", "some/file")
+        whenRunningCommand("add", "-t", "to/dir", "/home/user/some/file")
                 .thenItShould()
                 .fail()
-                .withErrorMessage("Missing required options and parameters: '--to=<to>', '<file>'")
+                .withErrorMessage(msg.argumentFromIsNotADirectory("home/doesnotexist"))
                 .withFileTreeDiff(Diff.unchanged());
     }
 
@@ -45,29 +47,30 @@ class AddCommandTest extends IntegrationTest {
         whenRunningCommand("add", "-f", "from/dir", "-t", "to/dir", "from/dir/some/file")
                 .thenItShould()
                 .succeed()
-                .withMessage(
-                        "Moving some/file from %s to %s and creating link",
-                        path("from/dir"),
-                        path("to/dir"))
-                .withMessage(
-                        "[MOVED] from/dir/some/file -> %s",
-                        path("to/dir/some/file"));
+                .withMessage(msg.init("some/file", "from/dir", "to/dir"))
+                .withMessage(msg.moved("from/dir/some/file", "to/dir/some/file"));
     }
-    // TODO add tests where some/file does not exist
-    // TODO add tests where some/file is not a subfile of from/dir
 
-    @Test
-    void shouldFail_whenDirectoryDoesNotExist() {
-        //given
-        given(env)
-                .withHome("home/doesnotexist");
-        //when/then
-        whenRunningCommand("add", "-t", "to/dir", "/home/user/some/file")
-                .thenItShould()
-                .fail()
-                .withErrorMessage(
-                        "Argument <from> (%s): must be an existing directory",
-                        home())
-                .withFileTreeDiff(Diff.unchanged());
+    @RequiredArgsConstructor
+    public static class AddMessageFactory {
+
+        @NonNull
+        private final Env env;
+
+        public String missingArguments() {
+            return "Missing required options and parameters: '--to=<to>', '<file>'";
+        }
+
+        public String argumentFromIsNotADirectory(String path) {
+            return String.format("Argument <from> (%s): must be an existing directory", env.path(path));
+        }
+
+        public String init(String name, String from, String to) {
+            return String.format("Moving %s from %s to %s and creating link", name, env.path(from), env.path(to));
+        }
+
+        public String moved(String from, String to) {
+            return String.format("[MOVED] %s -> %s", from, env.path(to));
+        }
     }
 }
