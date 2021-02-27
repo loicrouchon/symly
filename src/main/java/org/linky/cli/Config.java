de5db18613399c8cb64ceee447dfefcd53c1275f
Loic Rouchon <loic.rouchon@gmail.com>
2021-02-27 21:42:53 +0100

    Read configuration in a Map instead of a Property

--- src/main/java/org/linky/cli/Config.java
+++ src/main/java/org/linky/cli/Config.java
@@ -2,13 +2,15 @@ package org.linky.cli;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class Config {
 
     private static final String APPLICATION_PROPERTIES = "/application.properties";
 
-    private final Properties properties;
+    private final Map<String, String> properties;
 
     public Config() {
         this.properties = load();
@@ -23,17 +25,26 @@ public class Config {
     }
 
     private String property(String property) {
-        return properties.getProperty(property);
+        return properties.get(property);
     }
 
-    private static Properties load() {
-        InputStream is = VersionProvider.class.getResourceAsStream(APPLICATION_PROPERTIES);
-        try {
+    private static Map<String, String> load() {
+        try (InputStream is = Config.class.getResourceAsStream(APPLICATION_PROPERTIES)) {
             Properties properties = new Properties();
             properties.load(is);
-            return properties;
+            return properties.entrySet().stream()
+                    .collect(Collectors.toMap(
+                            e -> asString(e.getKey()),
+                            e -> asString(e.getValue())));
         } catch (IOException e) {
             throw new LinkyExecutionException("Unable to load application version", e);
         }
+    }
+
+    private static String asString(Object value) {
+        if (value == null) {
+            return null;
+        }
+        return value.toString();
     }
 }
