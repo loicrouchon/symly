65036ab3c22a687ee73aa46b4aa9570aac72a31d
Loic Rouchon <loic.rouchon@gmail.com>
2021-10-24 12:24:38 +0200

    Improve LinkTarget javadoc

--- src/main/java/org/symly/links/LinkTarget.java
+++ src/main/java/org/symly/links/LinkTarget.java
@@ -2,14 +2,15 @@ package org.symly.links;
 
 import java.nio.file.Path;
 
+/**
+ * <p>A {@link LinkTarget} represents a pair of {@link #name}/{@link #target}.
+ * The {@link #name} being relative to the root of the {@link Repository}.</p>
+ *
+ * @param name The name of the link as a relative {@link Path} from the root of the {@link Repository}.
+ * @param target The absolute {@link Path} pointed by the link.
+ */
 record LinkTarget(
-        /**
-         * The name of the link as a relative {@link Path} from the root of the {@link Repository}.
-         */
         Path name,
-        /**
-         * The absolute {@link Path} pointed by the link.
-         */
         Path target
 ) {
 
