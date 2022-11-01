1c8d5512f7eeb24575d4e0508b427ab85813b404
Loic Rouchon <loic@loicrouchon.com>
2022-11-01 17:12:45 +0100

    feat: print JVM version information in --version verbose mode

--- src/main/java/org/symly/cli/VersionProvider.java
+++ src/main/java/org/symly/cli/VersionProvider.java
@@ -1,5 +1,7 @@
 package org.symly.cli;
 
+import java.util.Arrays;
+import java.util.Objects;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import picocli.CommandLine;
@@ -12,8 +14,30 @@ public class VersionProvider implements CommandLine.IVersionProvider {
 
     @Override
     public String[] getVersion() {
-        return new String[] {
-            "%s version %s".formatted(config.applicationName(), config.applicationVersion()),
-        };
+        String symlyVersion = "%s version %s".formatted(config.applicationName(), config.applicationVersion());
+        if (config.verbose()) {
+            return new String[] {
+                symlyVersion,
+                "",
+                "-- JVM information --",
+                "%s %s".formatted(property("java.runtime.name"), property("java.version")),
+                "%s %s %s"
+                        .formatted(
+                                property("java.vendor"),
+                                property("java.vendor.version", "java.runtime.version"),
+                                property("java.version.date")),
+                "%s".formatted(property("java.home"))
+            };
+        } else {
+            return new String[] {symlyVersion};
+        }
+    }
+
+    private String property(String... names) {
+        return Arrays.stream(names)
+                .map(System::getProperty)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse("");
     }
 }
