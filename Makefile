12950d84876ad7e5d7eb108f7feb8976ef1d0be1
Loic Rouchon <loic@loicrouchon.com>
2022-12-17 17:30:47 +0100

    ci: Add GitHub actions check for documentation dirtiness

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 
-.PHONY: clean build release
+.PHONY: clean build release dirty-check
 
 build:
 	@./gradlew build --console=plain
 
 clean:
 	@./gradlew clean --console=plain
+
+dirty-check:
+	@./src/docs/resources/dirty-check.sh
 
 release:
 	@./src/build-tools/release.sh
---
+++ src/docs/resources/dirty-check.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+DIRS="docs src/docs"
+DIRTY_CHECK="git --no-pager diff --name-status --no-color ${DIRS}"
+
+if [ "$(${DIRTY_CHECK} | wc -l)" -gt 0 ]; then
+    echo "Dirty documentation is present."
+    echo "It is generated as part of the build."
+    echo "Please re-generate it and add it to source control"
+    echo ""
+    echo "Dirty files:"
+    ${DIRTY_CHECK}
+    exit 1
+fi
