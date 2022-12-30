9fd60d15a26abed9c7f9dde9b3f68b065a22e8a7
Loic Rouchon <loic@loicrouchon.com>
2022-12-30 19:48:33 +0100

    fix: Support non tty mode for releaser script

--- releaser/src/main/java/releaser/IO.java
+++ releaser/src/main/java/releaser/IO.java
@@ -5,6 +5,7 @@ import static releaser.Color.DEFAULT;
 import static releaser.Color.ERROR;
 
 import java.io.Console;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -12,10 +13,23 @@ import java.util.stream.Collectors;
 
 public class IO {
 
-    final Console console = System.console();
+    private final Console console = System.console();
+    private final PrintWriter writer;
+
+    @SuppressWarnings({
+        "java:S106" // Need to write to stdout
+    })
+    public IO() {
+        if (console != null) {
+            writer = console.writer();
+        } else {
+            writer = new PrintWriter(System.out);
+        }
+    }
 
     public IO printf(String message, Object... args) {
-        console.printf(message, args);
+        writer.printf(message, args);
+        writer.flush();
         return this;
     }
 
@@ -33,23 +47,21 @@ public class IO {
     public IO eprintln(Object message, Throwable t) {
         color(ERROR);
         println(message);
-        t.printStackTrace(console.writer());
+        t.printStackTrace(writer);
         resetColor();
         return this;
     }
 
     public IO color(Color color) {
-        console.printf(color.code);
-        return this;
+        return printf(color.code);
     }
 
     public IO resetColor() {
-        console.printf(DEFAULT.code);
-        return this;
+        return printf(DEFAULT.code);
     }
 
     public Optional<String> readLine(String message, Object... args) {
-        String input = console.readLine("> " + message, args);
+        String input = readLineFromConsole(message, args);
         if (input.isEmpty()) {
             return Optional.empty();
         }
@@ -62,8 +74,15 @@ public class IO {
         do {
             choices.forEach(choice -> printf(
                     "  %s) %s", CHOICE.str(choice.key()), choice.message().formatted()));
-            selection = console.readLine("> " + message, args);
+            selection = readLineFromConsole(message, args);
         } while (!validChoices.contains(selection));
         return selection;
+    }
+
+    private String readLineFromConsole(String message, Object[] args) {
+        if (console == null) {
+            throw new ReleaseFailure("Unable to read input from Console");
+        }
+        return console.readLine("> " + message, args);
     }
 }
--- releaser/src/main/java/releaser/ReleaseFailure.java
+++ releaser/src/main/java/releaser/ReleaseFailure.java
@@ -2,6 +2,10 @@ package releaser;
 
 class ReleaseFailure extends RuntimeException {
 
+    public ReleaseFailure(String message) {
+        super(message);
+    }
+
     public ReleaseFailure(String message, Throwable cause) {
         super(message, cause);
     }
